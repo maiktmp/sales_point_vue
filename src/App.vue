3163717b795b6343e5a937dc9042b9bd3a921dbe
Miguel <maiktmp@gmail.com>
2020-10-02 22:12:59 -0500

    Revert ":alien: Create new order and list with Firebase"
    This reverts commit 0e6f4f66

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,28 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer
-                v-model="drawer"
-                app
-        >
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon medium>add_box</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <router-link to="/create-order">Nueva Orden</router-link>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon medium>list_alt</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <router-link to="/create-order">Resumen de Órdenes</router-link>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-
-        <v-app-bar
-                app
-                color="indigo"
-                dark
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title>Administracíon</v-toolbar-title>
-        </v-app-bar>
-
-        <v-main>
-            <v-container fluid>
-                <v-row
-                        align="center"
-                        justify="center">
-                    <v-col>
-                        <router-view></router-view>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-main>
-        <v-footer
-                class="d-flex  justify-center"
-                color="indigo"
-                app>
-            <span class="white--text">Fecha: {{ date.toLocaleDateString("es-ES") }}</span>
-        </v-footer>
-    </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            source: String,
-        },
-        data: () => ({
-            drawer: false,
-            date: new Date
-        }),
-    }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/models/OrderStatus.js
+++
@@ -1,6 +0,0 @@
-export default {
-    CREATED: 1,
-    DELIVERED: 2,
-    PAID: 3,
-    CANCELED: 4,
-}
